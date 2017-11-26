aff85ff378fcdb611ecb90c9b4dd2ab80d79cdb1
archfan7411 <33993466+archfan7411@users.noreply.github.com>
2017-11-26 12:58:57 -0500

    Added mcop.py

---
+++ mcop.py
@@ -0,0 +1,25 @@
+import os
+files_removed = 0
+directory = input("Directory to clear players: ")
+for filename in os.listdir(directory):
+    file = open(filename, "r")
+    line = 18
+    while(line < 42):
+        test = file.read(i)
+        if test == "Empty":
+            i += 1
+        else:
+            print("Player", file, "not cleared")
+
+        file.close()
+        if line == 43:
+            print("Player", file, "deleted")
+            os.remove(filename)
+            files_removed += 1
+
+            break
+
+
+
+
+print("Player clearing process completed.", files_removed, "players deleted.")
